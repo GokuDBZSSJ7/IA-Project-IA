9d3a62971a4fbea063ea2f3d98be82db0c3f24e9
Lucas Baggio <lucas.baggio.silva@gmail.com>
2025-02-18 11:20:09 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load("model.pkl")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json()
+
+        formatted_data = {
+            "frequencia": float(data["frequencia"]), 
+            "performace": float(data["performace"]),
+            "mensalidade": float(data["mensalidade"]),
+            "renda_familiar": float(data["renda_familiar"]),
+            "atraso_pagamento": int(data["atraso_pagamento"]),
+            "possui_bolsa": int(data["possui_bolsa"]),
+            "distancia": float(data["distancia"])
+        }
+
+        df = pd.DataFrame([formatted_data])
+
+        expected_cols = ["frequencia", "performace", "mensalidade", "renda_familiar", "atraso_pagamento", "possui_bolsa", "distancia"]
+        df = df[expected_cols]
+
+        probabilidade = model.predict_proba(df)[:, 1][0]
+
+        if probabilidade < 0.4:
+            status = "Baixo"
+        elif probabilidade < 0.7:
+            status = "Médio"
+        else:
+            status = "Alto"
+        
+        return jsonify({"risk_score": round(probabilidade * 100, 2), "status": status})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
